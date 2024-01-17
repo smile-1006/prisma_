58511eaa7d75c32aa5080b314acbfff1726b1084
Smile <singhswapnil060303@gmail.com>
2024-01-17 19:02:57 +0530

    to getallfaculty from the database.

--- prismanextjs/app/faculty/index.jsx
+++ prismanextjs/app/faculty/index.jsx
@@ -1,11 +1,26 @@
+import { useGetFaculty } from '@/hooks/usegetfaculty'
 import React from 'react'
 
-function index() {
+function faculty() {
+  const {data: faculty,isloading, error} = useGetFaculty
   return (
-    <div>
-      
-    </div>
+    <>
+      <h1 className='text-4xl mt-3 p-4 text-center font-serif'>Add Faculty</h1>
+      <hr/>
+
+      <div className='container mx-auto mt-5 py-4'>
+        <div className='grid grid-cols-1 1g:grid-cols-3 md:grid-cols-2 item-center mt-3 py-3'>
+          {
+            faculty?.map((faculty)=>{
+              return <faculty key={faculty.id} faculty={faculty}/>
+            })
+          }
+        </div>
+
+      </div>
+
+    </>
   )
 }
 
-export default index
+export default faculty
